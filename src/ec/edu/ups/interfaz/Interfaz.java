116f90c6b049cfd327ac018648ad243bf1d47f76
Domenika  Delgado <>
2019-06-04 23:00:16 -0500

    Creacion de la clase Interfaz con sus metodos abstractos de ubicacion e informacion en  el paquete interfaz

---
+++ src/ec/edu/ups/interfaz/Interfaz.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.interfaz;
+
+/**
+ *
+ * @author Domenika Delgado
+ */
+public interface Interfaz {
+    
+    //Se refiere al lugar en donde se ecuentra 
+    public abstract String ubicacion();
+    
+    public abstract String informacion();
+ 
+}
